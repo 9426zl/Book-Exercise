558e8fde52550a625087929c45fc904f5a5dfec0
9426zl <qtce@163.com>
2015-02-19 19:19:38 +0800

    Revert "Revert "Core Python Programming""
    This reverts commit 811d19f235a52be9cae0958249b43bb2428d4997.

---
+++ CorePythonProgramming/Chapter02/ex05.py
@@ -0,0 +1,8 @@
+# (a)
+i = 0
+while i <= 10
+    print i
+
+# (b)
+for i in range(11)
+    print i
---
+++ CorePythonProgramming/Chapter02/ex07.py
@@ -0,0 +1,11 @@
+in_str = input("Enter a figure:")
+
+# for
+for item in in_str:
+    print item
+    
+# while
+i = 0
+in_len = len(in_str)
+while i < in_len:
+    print in_str[i]
