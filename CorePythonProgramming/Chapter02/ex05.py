0a5dfd11af20962061bfa284027bda0d5730c065
9426zl <qtce@163.com>
2015-02-19 19:23:36 +0800

    Revert "Revert "Revert "Core Python Programming"""
    This reverts commit 558e8fde52550a625087929c45fc904f5a5dfec0.

--- CorePythonProgramming/Chapter02/ex05.py
+++
@@ -1,8 +0,0 @@
-# (a)
-i = 0
-while i <= 10
-    print i
-
-# (b)
-for i in range(11)
-    print i
--- CorePythonProgramming/Chapter02/ex10.py
+++
@@ -1,8 +0,0 @@
-state = False
-while not state:
-    in_num = int(raw_input("Enter a number in 0 to 100:"))
-    if in_num in range(0, 101):
-        print "Done"
-        state = True
-    else:
-        print "Error"
