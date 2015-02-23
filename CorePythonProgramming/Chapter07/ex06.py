83703aac975e03ef428e86e906aaebf354bf4330
9426zl <qtce@163.com>
2015-02-24 01:02:46 +0800

    Ch07 ex06

--- CorePythonProgramming/Chapter07/ex06.py
+++ CorePythonProgramming/Chapter07/ex06.py
@@ -1,17 +1,27 @@
-
+import operator
 
 def sort_list(keys, stolist):
-    dict_key = {"S": 0, "A": 1, "P": 2, "C": 3}
-    n = dict_key(keys)
     key_s = []
     value_s = []
+    dict_key = {"S": 0, "A": 1, "P": 2, "C": 3}
+    title = ["StockSerial","Amount","PurchPrice","CurPrice"]
+    n = dict_key[keys]
+
     for elist in stolist:
         key_s.append(elist[n])
         del elist[n]
         value_s.append(elist)
-    stolist_s = zip(key_s, value_s)
-    sorted_list = sorted(stolist_s.items(), key=operator.itemgetter(0))
-    print sorted_list
+    stolist_s = dict(zip(key_s[:], value_s[:]))
+    # sorted_list = sorted(stolist_s.items(), key=operator.itemgetter(0))
+    key_s2 = sorted(stolist_s.keys())
+
+    print "%15s" % title[n],
+    del title[n]
+    print "%15s%15s%15s" % (title[0], title[1], title[2])
+    for key in key_s2:
+        print "%15s%15s%15s%15s" % (key, stolist_s.get(key)[0],stolist_s.get(key)[1], stolist_s.get(key)[2])
+    #print stolist_s
+    #print sorted_list
 
 
 total_list = []
