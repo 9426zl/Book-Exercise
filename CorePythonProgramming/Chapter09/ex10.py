856ca5cfd47af0a2769e09e81793d31215a34db3
9426zl <qtce@163.com>
2015-03-03 01:06:27 +0800

    Ch09 ex10

--- CorePythonProgramming/Chapter09/ex10.py
+++ CorePythonProgramming/Chapter09/ex10.py
@@ -1,6 +1,11 @@
 import os, random, struct, string
+
 from Tkinter import *
 import tkMessageBox
+import xlrd
+import xlwt
+from xlutils.copy import copy
+
 
 """Home Finances Application to manage savings, checking,
 money market, certificate of deposit(CD), and similar
@@ -13,50 +18,84 @@ class Application(Frame):
         self.grid()
         self.createwidgets()
 
+
     def createwidgets(self):
         "define the grid"
-        classChooseLabel = Label(self, text="Choose the Class: ")
-        classChooseLabel.grid(column=0, row=0, columnspan=2, sticky=W)
+        self.classChooseLabel = Label(self, text="Choose the Class: ")
+        self.classChooseLabel.grid(column=0, row=0, columnspan=2, sticky=W)
 
-        box_value = ["Savings", "Checking", "Money Market", "Certificate of Deposit"]
+        self.box_value = ["Savings", "Checking", "Money Market", "Certificate of Deposit"]
         self.var = StringVar()
-        self.var.set(box_value[0])
-        self.classChoosebox = OptionMenu(self, self.var, "Savings","Checking",\
-                                         "Money Market","Certificate of Deposit")            # how to use box_value
+        self.var.set(self.box_value[0])
+        self.classChoosebox = OptionMenu(self, self.var, "Savings", "Checking",\
+                                         "Money Market", "Certificate of Deposit")            # how to use box_value
         self.classChoosebox.grid(column=2, row=0, sticky='WE')
 
-        expenLabel = Label(self, text="Expenditure: ")
-        expenLabel.grid(column=0, row=1, columnspan=2, sticky=W)
+        self.expenLabel = Label(self, text="Expenditure: ")
+        self.expenLabel.grid(column=0, row=1, columnspan=2, sticky=W)
         self.expenEntry = Entry(self)
-        self.expenEntry.insert(1, 0)
         self.expenEntry.grid(column=2, row=1)
 
-        incomeLabel = Label(self, text="Income:")
-        incomeLabel.grid(column=0, row=2, columnspan=2, sticky=W)
+        self.incomeLabel = Label(self, text="Income:")
+        self.incomeLabel.grid(column=0, row=2, columnspan=2, sticky=W)
         self.incomeEntry = Entry(self)
-        self.incomeEntry.insert(1, 0)
         self.incomeEntry.grid(column=2, row=2)
 
-        doneButton = Button(self, text="Done", command=self.savedetail)
-        doneButton.grid(column=0, row=3, sticky='WE')
+        self.doneButton = Button(self, text="Done", command=self.savedetail)
+        self.doneButton.grid(column=0, row=3, sticky='WE')
 
-        backButton = Button(self, text="Back", command=self.withdrawact)
-        backButton.grid(column=1, row=3, sticky='WE')
-        backButton.grid(column=1, row=3, sticky='WE')
+        self.backButton = Button(self, text="Back", command=self.withdrawact)
+        self.backButton.grid(column=1, row=3, sticky='WE')
+        self.backButton.grid(column=1, row=3, sticky='WE')
 
-        showButton = Button(self, text="Show the estate", command=self.showitem)
-        showButton.grid(column=2, row=3, sticky='WE')
+        self.showButton = Button(self, text="Show the estate", command=self.showitem)
+        self.showButton.grid(column=2, row=3, sticky='WE')
+
+        self.msum = {"Savings": 0, "Checking": 0, "Money Market": 0,
+                     "Certificate of Deposit": 0}
+
 
     def savedetail(self):
         "define the function which saves the item of the account"
-        self.msum = {"Savings": 0, "Checking": 0, "Money Market": 0,
-                    "Certificate of Deposit": 0}
         kind = self.var.get()
-        exp = float(self.expenEntry.get())
-        inc = float(self.incomeEntry.get())
-        self.msum[kind] = self.msum[kind] + inc - exp
+        ind = int(self.box_value.index(kind))
+        exp = self.expenEntry.get()
+        inc = self.incomeEntry.get()
+
+        if exp.isdigit():
+            exp = float(exp)
+        else:
+            exp = 0
+        if inc.isdigit():
+            inc = float(inc)
+        else:
+            inc = 0
+
+        self.msum[kind] = inc - exp
         tkMessageBox.showinfo("Message", "The change has been changed into %s\nex:%5.2f in:%5.2f \
 and the sum is:%6.2f" % (kind, exp, inc, self.msum[kind]))
+        self.expenEntry.delete(0, END)
+        self.incomeEntry.delete(0, END)
+
+        'Save to the xls'
+        stfile = xlrd.open_workbook('HomeFinance.xls', formatting_info=True)
+        r_sheet = stfile.sheet_by_index(0)
+        r = r_sheet.nrows
+        lin = 2*ind
+        wb = copy(stfile)
+        sheet = wb.get_sheet(0)
+        if r == 3:
+            totalval = float(self.msum[kind])
+        else:
+            totalval = r_sheet.cell(r-1, 9).value
+        print totalval
+        totalval += float(self.msum[kind])
+        sheet.write(r, lin, inc)
+        sheet.write(r, lin+1, exp)
+        sheet.write(r, 8, float(self.msum[kind]))
+        sheet.write(r, 9, totalval)
+        wb.save('HomeFinance.xls')
+
 
     def withdrawact(self):
         "define the function which withdraws the action"
@@ -66,11 +105,18 @@ and the sum is:%6.2f" % (kind, exp, inc, self.msum[kind]))
         self.msum[kind] = self.msum[kind] - inc + exp
         tkMessageBox.showinfo("Message", "The change has been withdraw %s\nex:%5.2f in:%5.2f \
 and the sum is:%6.2f" % (kind, exp, inc, self.msum[kind]))
+        self.expenEntry.delete(0, END)
+        self.incomeEntry.delete(0, END)
+
 
     def showitem(self):
-        "define the funtion shows the whole account"
-        pass
-
+        "define the function shows the whole account"
+        s = self.msum
+        tkMessageBox.showinfo("Message", "Savings: %.2f\n Checking: %.2f\n \
+Money Market: %.2f\n Certificate of Deposit: %.2f" % (s["Savings"], s["Checking"], s["Money Market"],
+s["Certificate of Deposit"]))
+        self.expenEntry.delete(0, END)
+        self.incomeEntry.delete(0, END)
 
 
 app = Application()
