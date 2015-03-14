3e00f66b9a288ad5207706c67bdc97948e289b0b
9426zl <qtce@163.com>
2015-03-14 23:46:05 +0800

    Ch15 ex16

---
+++ CorePythonProgramming/Chapter15/ex16.py
@@ -0,0 +1,29 @@
+from random import randint, choice
+from string import lowercase
+from sys import maxint
+from time import ctime
+import os
+
+doms = ('com', 'edu', 'net', 'org', 'gov')
+
+for i in range(randint(5, 10)):
+    dtint = randint(0, maxint-1) # pick date
+    dtstr = ctime(dtint) # date string
+
+shorter = randint(4, 7) # login shorter
+em = ''
+for j in range(shorter): # generate login
+    em += choice(lowercase)
+
+longer = randint(shorter, 12) # domain longer
+dn = ''
+for j in range(longer): # create domain
+    dn += choice(lowercase)
+
+print '%s::%s@%s.%s::%d-%d-%d' % (dtstr, em,
+                                  dn, choice(doms), dtint, shorter, longer)
+si = dtstr+ '::' + em + '@' + dn + '.'+ choice(doms) + '::' \
+     + 'dtint' + '-'+ 'shorter' + '-'+ 'longer'
+f = open('redata.txt', 'a')
+f.write(si)
+f.close()
