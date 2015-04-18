3d27a5b0db341bf9c7dc24ff9eed6a4b34cbc0f1
9426zl <qtce@163.com>
2015-04-18 23:53:24 +0800

    Chapter 03

--- TheCProgrammingLanguage/Chapter03/ex03.c
+++ TheCProgrammingLanguage/Chapter03/ex03.c
@@ -5,12 +5,13 @@ void expand(char s1[], char s2[])
     int i, j;
     
     i = j = 0;
-    while((c = s1[i++]) != '\0')
+    while((c = s1[i++]) != '\0'){
         if (s1[i] == '-' && s1[i+1] >= c){
             i++;
             while(c < s1[i])
                 s2[j++] = c++;
         } else
             s2[j++] = c;
+    }
     s2[j] = '\0';
 }
