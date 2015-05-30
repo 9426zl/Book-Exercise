d9bb192f0fd0c4c4c695cd96e4de012c6b2759b3
9426zl <qtce@163.com>
2015-05-30 18:00:42 +0800

    Chapter08

---
+++ C++primer/Chapter08/ex02.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+istream& test_cin(istream &is)
+{
+    string buf;
+    while (is >> buf)
+        cout << buf << endl;
+    is.clear();
+    return is;
+}
+
+int main()
+{
+    istream& is = test_cin(cin);
+    cout << is.rdstate() << endl;
+    return 0;
+}
---
+++ C++primer/Chapter08/ex05.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void ReadFile(const string& fileName, vector<string>& sVec)
+{
+	string buf;
+
+	ifstream input(fileName);
+
+	if(input)
+	{
+		while(input >> buf)
+			sVec.push_back(buf);
+	}
+}
+
+
+int main()
+{
+	vector<string> svec;
+	decltype(svec.size()) index, i = 0;
+	
+	ReadFile("exer.txt", svec);
+
+	index = svec.size();
+	for(; i !=index; ++i)
+		cout << svec[i] << endl;
+
+	return 0;
+}
+		
