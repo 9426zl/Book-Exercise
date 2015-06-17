910dc8776128d6392822c82dd88b23f00a14198a
9426zl <qtce@163.com>
2015-06-18 00:19:38 +0800

    Chapter07

--- C++primer/Chapter07/ex21.h
+++ C++primer/Chapter07/ex21.h
@@ -1,14 +1,20 @@
 //
-// Chapter07 Exercise11 head file
+// Chapter07 Exercise21
 //
 
-#ifndef ex11_h
-#define ex11_h
+#ifndef ex21_h
+#define ex21_h
 
 #include <iostream>
 #include <string>
 
 struct Sales_data {
+	
+	friend std::istream &read(std::istream &is, Sales_data &item);
+	friend std::ostream &print(std::ostream &os, const Sales_data &item);
+	friend Sales_data add(const Sales_data &lhs, const Sales_data &rhs);
+
+public:
 	Sales_data() = default;
 	Sales_data(const std::string &s):bookNo(s){ }
 	Sales_data(const std::string &s, unsigned n, double p):
@@ -19,6 +25,7 @@ struct Sales_data {
 	Sales_data& combine(const Sales_data&);
 	double avg_price() const;
 
+private:
 	std::string bookNo;
 	unsigned units_sold = 0;
 	double revenue = 0.0;
